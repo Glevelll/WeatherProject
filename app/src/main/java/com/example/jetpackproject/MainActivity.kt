e3db05bc5fb79432828db5928da78b0a324ad5af
gleb <gleb.nikishin.2003@gmail.com>
2024-04-07 01:37:44 +0300

    Добавлены horizontalPager, картинка и авторизация

--- app/src/main/java/com/example/jetpackproject/MainActivity.kt
+++ app/src/main/java/com/example/jetpackproject/MainActivity.kt
@@ -3,44 +3,209 @@ package com.example.jetpackproject
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Tab
+import androidx.compose.material3.TabRow
+import androidx.compose.material3.TabRowDefaults
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.semantics.Role.Companion.Button
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.jetpackproject.ui.theme.JetpackProjectTheme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             JetpackProjectTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+                Box(modifier = Modifier.fillMaxSize()) {
+                    PagerScreen()
                 }
             }
         }
     }
 }
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun PagerScreen() {
+    val coroutineScope = rememberCoroutineScope()
+    val pagerState = rememberPagerState{2}
+
+    Column {
+        TabRow(selectedTabIndex = pagerState.currentPage, modifier = Modifier.height(40.dp)) {
+            Tab(
+                selected = pagerState.currentPage == 0,
+                modifier = Modifier.height(40.dp),
+                onClick = { coroutineScope.launch { pagerState.scrollToPage(0) } }
+            ) {
+                Text(text = "Вход")
+            }
+            Tab(
+                selected = pagerState.currentPage == 1,
+                modifier = Modifier.height(40.dp),
+                onClick = { coroutineScope.launch { pagerState.scrollToPage(1) } }
+            ) {
+                Text(text = "Регистрация")
+            }
+        }
+
+        HorizontalPager(state = pagerState) { page ->
+            when (page) {
+                0 -> LoginPage()
+                1 -> RegisterPage()
+            }
+        }
+        TabRowDefaults.Indicator(color = Color.Blue, height = 2.dp)
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    JetpackProjectTheme {
-        Greeting("Android")
+fun LoginPage() {
+    val maxChar = 18
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 20.dp),
+            contentAlignment = Alignment.Center
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.jci),
+                contentDescription = "Image",
+                modifier = Modifier.size(70.dp)
+            )
+        }
+        Text(text = "Вход", fontSize = 30.sp, color = Color.Black)
+
+        var user by remember { mutableStateOf("") }
+        var pass by remember { mutableStateOf("") }
+
+        val passwordVisible by rememberSaveable() { mutableStateOf(false) }
+        TextField(value = user, { text->user=text},
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(66.dp)
+                .padding(start = 64.dp, end = 64.dp, top = 8.dp, bottom = 8.dp)
+                .border(
+                    1.dp,
+                    Color(android.graphics.Color.parseColor("#7d32a8")),
+                    shape = RoundedCornerShape(50)
+                ),
+            shape = RoundedCornerShape(50),
+            textStyle = TextStyle(
+                textAlign = TextAlign.Center,
+                fontSize = 14.sp
+            ),
+            colors = TextFieldDefaults.colors(
+                focusedIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent,
+            ),
+            maxLines = maxChar
+        )
+
+        TextField(value = pass, { text->pass=text},
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(66.dp)
+                .padding(start = 64.dp, end = 64.dp, top = 8.dp, bottom = 8.dp)
+                .border(
+                    1.dp,
+                    Color(android.graphics.Color.parseColor("#FFD0BCFF")),
+                    shape = RoundedCornerShape(50)
+                ),
+            shape = RoundedCornerShape(50),
+            textStyle = TextStyle(
+                textAlign = TextAlign.Center,
+                fontSize = 14.sp
+            ),
+            colors = TextFieldDefaults.colors(
+                focusedIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent,
+            ),
+            visualTransformation = if (passwordVisible) VisualTransformation.None else PasswordVisualTransformation(),
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+            maxLines = maxChar
+        )
+
+        Button(onClick = {/*TODO*/},
+            Modifier
+                .fillMaxWidth()
+                .height(66.dp)
+                .padding(start = 64.dp, end = 64.dp, top = 8.dp, bottom = 8.dp),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color.Blue
+            ),
+            shape = RoundedCornerShape(50)
+        ) {
+            Text(
+                text = "Войти",
+                color = Color.White,
+                fontSize = 18.sp
+            )
+        }
+    }
+}
+
+@Composable
+fun RegisterPage() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 20.dp),
+            contentAlignment = Alignment.Center
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.jci),
+                contentDescription = "Image",
+                modifier = Modifier.size(70.dp)
+            )
+        }
+        Text(text = "Регистрация", fontSize = 24.sp, color = Color.Black)
     }
 }
