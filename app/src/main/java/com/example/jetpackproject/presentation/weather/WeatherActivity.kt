06afaaf707b9df0abd8629553c04070d5d70ece8
gleb <gleb.nikishin.2003@gmail.com>
2024-07-22 17:47:16 +0400

    Решение проблемы взаимодействия сервиса и активностей

--- app/src/main/java/com/example/jetpackproject/presentation/weather/WeatherActivity.kt
+++ app/src/main/java/com/example/jetpackproject/presentation/weather/WeatherActivity.kt
@@ -1,34 +1,41 @@
-package com.example.jetpackproject.presentation
+package com.example.jetpackproject.presentation.weather
 
 import android.Manifest
-import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.result.ActivityResultLauncher
-import androidx.activity.result.contract.ActivityResultContract
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import androidx.glance.layout.Column
+import com.example.jetpackproject.presentation.authorization.MainActivity
 import com.example.jetpackproject.presentation.theme.JetpackProjectTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class WeatherActivity : ComponentActivity() {
+
     private val viewModel: WeatherViewModel by viewModels()
     private lateinit var permissionLauncher: ActivityResultLauncher<Array<String>>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         permissionLauncher = registerForActivityResult(
@@ -36,17 +43,16 @@ class WeatherActivity : ComponentActivity() {
         ) {
             viewModel.loadWeatherInfo()
         }
-
         permissionLauncher.launch(arrayOf(
             Manifest.permission.ACCESS_FINE_LOCATION,
-            Manifest.permission.ACCESS_COARSE_LOCATION
+            Manifest.permission.ACCESS_COARSE_LOCATION,
         ))
         setContent {
             JetpackProjectTheme {
                 Box(
                     modifier = Modifier.fillMaxSize()
                 ) {
-                    androidx.compose.foundation.layout.Column(
+                    Column(
                         modifier = Modifier
                             .fillMaxSize()
                             .background(Color.Gray)
@@ -70,6 +76,21 @@ class WeatherActivity : ComponentActivity() {
                             textAlign = TextAlign.Center,
                             modifier = Modifier.align(Alignment.Center)
                         )
+                    }
+
+                    Button(
+                        onClick = {
+                            val intent = Intent(this@WeatherActivity, MainActivity::class.java)
+                            startActivity(intent)
+                            finish()
+                        },
+                        modifier = Modifier
+                            .align(Alignment.BottomCenter)
+                            .padding(bottom = 20.dp)
+                            .size(width = 200.dp, height = 50.dp)
+                            .clip(RoundedCornerShape(50.dp))
+                    ) {
+                        Text(text = "Выйти")
                     }
                 }
             }
